23e187663f45f8a7ecd27ed2ce77c62c6ee424e1
fluctlight-kayaba <kayaba.fluctlight@gmail.com>
2023-05-31 16:56:31 +0700

    custom user commands, BufferKill

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,4 +1,5 @@
 require("utils.settings")
+require("utils.commands")
 require("utils.autocmds")
 require("utils.keymaps")
 require("core.plugins")
---
+++ nvim/lua/utils/commands.lua
@@ -0,0 +1,13 @@
+local opts = { force = true }
+local commands = {
+	{
+		name = "BufferKill",
+		fn = function()
+			require("core.plugins.bufferline").buf_kill("bd")
+		end,
+	},
+}
+
+for _, cmd in pairs(commands) do
+	vim.api.nvim_create_user_command(cmd.name, cmd.fn, opts)
+end
