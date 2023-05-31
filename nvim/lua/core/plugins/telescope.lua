c98eb405a12ed7865fe55498cce6efc4189790d6
fluctlight-kayaba <kayaba.fluctlight@gmail.com>
2023-05-31 17:23:58 +0700

    load fzf in telescope

--- nvim/lua/core/plugins/telescope.lua
+++ nvim/lua/core/plugins/telescope.lua
@@ -1,7 +1,14 @@
 local M = {}
 
 local options = {
-
+	extensions = {
+		fzf = {
+			fuzzy = true,                -- false will only do exact matching
+			override_generic_sorter = true, -- override the generic sorter
+			override_file_sorter = true, -- override the file sorter
+			case_mode = "smart_case",    -- or "ignore_case" or "respect_case"
+		},
+	},
 }
 
 M.configure = function()
