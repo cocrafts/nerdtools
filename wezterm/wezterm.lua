c1b749fafefbf1998c5a17ad71c77386114488fd
fluctlight-kayaba <kayaba.fluctlight@gmail.com>
2023-06-23 06:57:00 +0700

    fix: wezterm icon scaling

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,4 +1,5 @@
 local wezterm = require("wezterm")
+local action = wezterm.action
 
 local config = {}
 local colors = {
@@ -6,6 +7,7 @@ local colors = {
 	bg = "#1A1B26",
 	dark = "#16161e",
 	darker = "#0c0c0f",
+	cursor = "#C0CAF7",
 }
 
 if wezterm.config_builder then
@@ -17,9 +19,17 @@ config.initial_cols = 100
 
 config.color_scheme = "tokyonight_night"
 config.font_size = 14.4
-config.font = wezterm.font {
-	family = "OperatorMonoLig Nerd Font Mono",
-	weight = "Book",
+config.use_cap_height_to_scale_fallback_fonts = true
+config.font = wezterm.font_with_fallback {
+	{
+		family = "OperatorMonoLig Nerd Font Mono",
+		weight = "Book"
+	},
+	{
+		family = "Symbols Nerd Font Mono",
+		scale = 0.5,
+		weight = "Book"
+	},
 }
 
 config.hide_tab_bar_if_only_one_tab = true
@@ -31,6 +41,20 @@ config.window_padding = {
 	right = 0,
 	top = 0,
 	bottom = 0,
+}
+
+config.keys = {
+	{
+		key = "K",
+		mods = "CMD|SHIFT",
+		action = action.ClearScrollback "ScrollbackAndViewport",
+	},
+}
+
+config.colors = {
+	cursor_bg = colors.cursor,
+	cursor_fg = colors.darker,
+	cursor_border = colors.cursor,
 }
 
 return config
