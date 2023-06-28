5b505330d50ca9138f39c1812aa6319166771344
fluctlight-kayaba <kayaba.fluctlight@gmail.com>
2023-06-28 17:38:08 +0700

    remap toggleterm clearrr hotkey

--- nvim/lua/core/plugins/toggleterm.lua
+++ nvim/lua/core/plugins/toggleterm.lua
@@ -35,12 +35,12 @@ local options = {
 
 function _G.set_terminal_keymaps()
   local opts = {buffer = 0}
-  -- vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
   vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
   vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
   vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
   vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
-  vim.keymap.set('t', '<C-l>', [[<Cmd>TermExec cmd="clear"<CR>]], opts)
+  vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
+  vim.keymap.set('t', '<C-c>', [[<Cmd>TermExec cmd="clear"<CR>]], opts)
   vim.keymap.set('t', '<C-w>', [[<Cmd>TermExec cmd="exit"<CR>]], opts)
 end
 
