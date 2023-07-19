12ec28b1e902de584deb0c7676858eaca6930fea
Fluctlight Kayaba <kayaba.fluctlight@gmail.com>
2023-07-20 04:38:29 +0700

    ansible: install rbenv, ruby-build

--- zsh/entry.sh
+++ zsh/entry.sh
@@ -14,5 +14,6 @@ source $ZSH/oh-my-zsh.sh
 source ~/nerdtools/zsh/keybindings.sh
 
 export STARSHIP_CONFIG=~/nerdtools/conf/starship.toml
+eval "$(rbenv init - zsh)"
 eval "$(starship init zsh)" # load starship theme
 eval "$(zoxide init zsh)"
