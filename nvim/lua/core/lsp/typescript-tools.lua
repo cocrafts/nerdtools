565d8756ec0b0f4b28d69aac68dc0b2102fc8f94
Fluctlight Kayaba <kayaba.fluctlight@gmail.com>
2023-09-02 19:04:07 +0700

    typescript: support inlay-hints

--- nvim/lua/core/lsp/typescript-tools.lua
+++ nvim/lua/core/lsp/typescript-tools.lua
@@ -2,35 +2,19 @@ local config = require("utils.config")
 local M = {}
 
 M.configure = function()
-	-- local options = {
-	-- 	settings = {
-	-- 		typescript = {},
-	-- 		javascript = {},
-	-- 	},
-	-- }
-
-	-- if config.use_inlay_hints then
-	-- 	local hint_options = {
-	-- 		includeInlayParameterNameHints = "all",
-	-- 		includeInlayParameterNameHintsWhenArgumentMatchesName = false,
-	-- 		includeInlayFunctionParameterTypeHints = true,
-	-- 		includeInlayVariableTypeHints = true,
-	-- 		includeInlayVariableTypeHintsWhenTypeMatchesName = false,
-	-- 		includeInlayPropertyDeclarationTypeHints = true,
-	-- 		includeInlayFunctionLikeReturnTypeHints = true,
-	-- 		includeInlayEnumMemberValueHints = true,
-	-- 	}
-
-	-- 	options.settings.typescript["inlayHints"] = hint_options
-	-- 	options.settings.javascript["inlayHints"] = hint_options
-	-- end
-
-	-- lspconfig.tsserver.setup(options)
-
 	require("typescript-tools").setup({
 		settings = {
 			separate_diagnostic_server = true,
 			publish_diagnostic_on = "change",
+			tsserver_file_preferences = {
+				includeInlayParameterNameHints = "all",
+				includeCompletionsForModuleExports = true,
+				quotePreference = "auto",
+			},
+			tsserver_format_options = {
+				allowIncompleteCompletions = false,
+				allowRenameOfImportPath = false,
+			},
 		},
 	})
 end
