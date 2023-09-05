b0e43b436bf508f0d891432e71a1d60f71f11dab
Fluctlight Kayaba <kayaba.fluctlight@gmail.com>
2023-09-06 01:40:57 +0700

    use go plugin's built-in inlayHints

--- nvim/lua/core/lsp/go.lua
+++ nvim/lua/core/lsp/go.lua
@@ -1,17 +1,13 @@
 local M = {}
 
-M.configure = function(lspconfig)
+M.configure = function()
 	require("go").setup({
-		lsp_cfg = false,
 		lsp_gofumpt = true,
 		lsp_keymaps = false,
 		lsp_inlay_hints = {
-			enabled = false,
+			enabled = true,
 		},
 	})
-
-	local config = require("go.lsp").config()
-	lspconfig.gopls.setup(config)
 end
 
 return M
