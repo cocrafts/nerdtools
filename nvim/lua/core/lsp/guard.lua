55dbf96c5e1ecdab76be44ead8dde76a44c288c8
Fluctlight Kayaba <kayaba.fluctlight@gmail.com>
2023-09-23 17:42:08 +0700

    clang formatter/linter using guard.nvim

--- nvim/lua/core/lsp/guard.lua
+++ nvim/lua/core/lsp/guard.lua
@@ -105,6 +105,24 @@ M.configure = function()
 		}),
 	})
 
+	ft("c"):fmt({
+		cmd = "clang-format",
+		stdin = true,
+	}):lint({
+		cmd = "clang-tidy",
+		args = { "--quiet" },
+		parse = lint.from_regex({
+			source = "clang-tidy",
+			regex = ":(%d+):(%d+):%s+(%w+):%s+(.-)%s+%[(.-)%]",
+			groups = { "lnum", "col", "severity", "message", "code" },
+			severities = {
+				information = lint.severities.info,
+				hint = lint.severities.info,
+				note = lint.severities.style,
+			},
+		}),
+	})
+
 	ft("zig"):fmt({
 		cmd = "zigfmt",
 		args = { "fmt", "--stdin" },
