707a15354176671f680b6e25e8d3abe46d1d14b0
Fluctlight Kayaba <kayaba.fluctlight@gmail.com>
2024-02-17 14:01:13 +0700

    use nimls instead of nim_langserver as it is faster

--- nvim/lua/core/lsp/nim.lua
+++ nvim/lua/core/lsp/nim.lua
@@ -1,13 +1,7 @@
 local M = {}
 
 M.configure = function(lspconfig)
-	lspconfig.nim_langserver.setup({
-		settings = {
-			nim = {
-				nimsuggestPath = "~/.nimble/bin/nimsuggest",
-			},
-		},
-	})
+	lspconfig.nimls.setup({})
 end
 
 return M
