309fa4406e61f9f607899ced127cc4fc41dd699b
Fluctlight Kayaba <kayaba.fluctlight@gmail.com>
2024-03-28 20:36:39 +0700

    instant gitblame feedback

--- nvim/lua/utils/settings.lua
+++ nvim/lua/utils/settings.lua
@@ -5,6 +5,7 @@ local globals = {
 	gitblame_date_format = "%r",
 	gitblame_message_template = " <author>, <date> • <summary> ",
 	gitblame_message_when_not_committed = "",
+	gitblame_delay = 0,
 	zig_fmt_autosave = 0,
 }
 
