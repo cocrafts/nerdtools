dffb7cd54e1f14def116525b65509e75b3b9d3c5
Fluctlight Kayaba <kayaba.fluctlight@gmail.com>
2024-05-16 14:24:38 +0700

    feat: move/switch buffer after split

--- nvim/lua/utils/keymaps.lua
+++ nvim/lua/utils/keymaps.lua
@@ -9,8 +9,8 @@ key.mode_keys("n", {
 	["<C-k>"] = "<C-w>k",
 	["<C-l>"] = "<C-w>l",
 
-	["<C-x>"] = ":split<CR>",
-	["<C-v>"] = ":vsplit<CR>",
+	["<C-x>"] = ":split<CR>:b#<CR>",
+	["<C-v>"] = ":vsplit<CR>:b#<CR>",
 
 	["<TAB>"] = ":bprev<CR>",
 	["<S-TAB>"] = ":bnext<CR>",
@@ -41,13 +41,13 @@ key.mode_keys("v", {
 })
 
 if vim.g.neovide then
-	vim.g.neovide_input_use_logo = 1 -- enable use of the logo (cmd) key
-	vim.keymap.set("n", "<D-s>", ":up<CR>") -- Save
-	vim.keymap.set("v", "<D-c>", '"+y') -- Copy
-	vim.keymap.set("n", "<D-v>", '"+P') -- Paste normal mode
-	vim.keymap.set("v", "<D-v>", '"+P') -- Paste visual mode
-	vim.keymap.set("c", "<D-v>", "<C-R>+") -- Paste command mode
-	vim.keymap.set("i", "<D-v>", '<ESC>l"+Pli') -- Paste insert mode
+	vim.g.neovide_input_use_logo = 1                                                -- enable use of the logo (cmd) key
+	vim.keymap.set("n", "<D-s>", ":up<CR>")                                         -- Save
+	vim.keymap.set("v", "<D-c>", '"+y')                                             -- Copy
+	vim.keymap.set("n", "<D-v>", '"+P')                                             -- Paste normal mode
+	vim.keymap.set("v", "<D-v>", '"+P')                                             -- Paste visual mode
+	vim.keymap.set("c", "<D-v>", "<C-R>+")                                          -- Paste command mode
+	vim.keymap.set("i", "<D-v>", '<ESC>l"+Pli')                                     -- Paste insert mode
 	vim.api.nvim_set_keymap("!", "<D-v>", "<C-R>+", { noremap = true, silent = true }) -- Allow insert mode paste
 
 	-- Runtime font scale
