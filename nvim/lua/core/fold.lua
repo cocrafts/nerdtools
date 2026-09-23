fface5fd19cfeeee332dda569f0a2ee13e8bedd7
Son Le <fluctlight-kayaba@users.noreply.github.com>
2026-09-24 01:04:07 +0700

    chore(nvim): drop dead lspconfig loop in fold

--- nvim/lua/core/fold.lua
+++ nvim/lua/core/fold.lua
@@ -39,19 +39,6 @@ M.configure = function()
 	vim.o.foldlevelstart = 99                               -- do not close folds when a buffer is opened
 	vim.o.foldenable = true
 
-	local capabilities = vim.lsp.protocol.make_client_capabilities()
-	capabilities.textDocument.foldingRange = {
-		dynamicRegistration = false,
-		lineFoldingOnly = true,
-	}
-	local language_servers = vim.lsp.get_clients() -- or list servers manually like {'gopls', 'clangd'}
-	for _, ls in ipairs(language_servers) do
-		require("lspconfig")[ls].setup({
-			capabilities = capabilities,
-			-- you can add other fields for setting up lsp server in this table
-		})
-	end
-
 	require("ufo").setup({
 		fold_virt_text_handler = handler,
 	})
